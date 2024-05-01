9403f9a5c88028c9fc90154a9d7b95c87f5a99be
Andrew Moreira <88143161+AndrewMoreira91@users.noreply.github.com>
2024-04-30 23:49:27 -0300

    Atualiza descrição do aplicativo no arquivo README.md Adiciona funcionalidades de progresso e diálogo modal no arquivo main.py Atualiza dependências no arquivo requirements.txt

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# A imagerotate Flet app
+##Image Rotate Flet App
+Este é um aplicativo simples para verificar se uma imagem está na orientação correta e, se não estiver, girá-la. Foi desenvolvido em Python utilizando a biblioteca Flet, que é baseada no Flutter.
 
-An example of a minimal Flet app.
-
-To run the app:
-
-```
-flet run [app_directory]
-```
+#Motivação
+Este projeto foi criado com o intuito de resolver um problema enfrentado no meu trabalho atual. Como parte das minhas responsabilidades, realizo fotografias de vistoria de imóveis, e ocasionalmente algumas dessas fotos ficam com a orientação incorreta. Para aumentar a eficiência do processo de correção dessas imagens, desenvolvi este aplicativo.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flet as ft
 from PIL import Image
+import time
 
 def main(page: ft.Page):
     page.title = "Image Rotate"
@@ -8,54 +9,43 @@ def main(page: ft.Page):
     page.window_resizable = True
     page.window_width = 800
     page.window_height = 650
+    page.window_min_width = 600
+    page.window_min_height = 600
 
     files = []
-
-    def rotate_images(degrees: int):
-        number_files_rotate = 0
-        print(f"Rotating images {degrees} degrees")
-        if len(files) == 0:
-            return
-        for file in files:
-            try:
-                img = Image.open(file.path)
-                width, height = img.size
-                if width < height:
-                    img.rotate(degrees, expand=True).save(file.path)
-                    print(f"Image {file.name} rotated {degrees} degrees")
-                    number_files_rotate += 1
-            except Exception as e:
-                print(f"An error occurred: {e}")
-        if number_files_rotate == 0:
-            return "Nenhuma imagem rotacionada!"
-        else:
-            return str(number_files_rotate) + " imagens rotacionadas com sucesso!"
-
-    def handle_click(e):
-        response = rotate_images(90)
-        open_dialog_modal(response)
-        files.clear()
-
-    def update_text_number_files():
-        if len(files) == 0:
-            textNumberFiles.value = "Nenhuma imagem selecionada"
-            buttonRotate.disabled = True
-        elif len(files) >= 1:
-            textNumberFiles.value = f"{len(files)} imagens selecionadas"
-            buttonRotate.disabled = False
-        page.update()
-
-    def open_dialog_modal(content: str):
-        dlg_modal.content = ft.Text(content)
-        page.dialog = dlg_modal
-        dlg_modal.open = True
-        page.update()
-
+    
+    text_select_images_count = ft.Text(
+        "Nenhuma imagem selecionada",
+        size=24,
+        color=ft.colors.GREY_500,
+        weight=ft.FontWeight.BOLD,
+    )
+    
+    text_rotated_images_count = ft.Text(
+        "0 imagens rotacionadas",
+        size=16,
+        color=ft.colors.GREY_600,
+        visible=False
+    )
+    
+    progress_bar = ft.ProgressBar(
+        width=500, bgcolor=None, color=ft.colors.CYAN_500, visible=True
+    )
+    
+    conteiner_pb = ft.Column([
+        progress_bar,
+        ft.Text("Rotacionando imagens...", size=16, color=ft.colors.BLACK)
+    ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        visible=False,
+        height=50
+    )
+    
     def close_dlg(e):
         dlg_modal.open = False
         update_text_number_files()
         page.update()
-
+    
     dlg_modal = ft.AlertDialog(
         modal=True,
         title=ft.Text("Imagens Rotacionadas com Sucesso!"),
@@ -65,6 +55,60 @@ def main(page: ft.Page):
         actions_alignment=ft.MainAxisAlignment.END,
         on_dismiss=lambda e: print("Modal dialog dismissed!"),
     )
+
+    def rotate_images(degrees: int):
+        images_rotated_count = 0
+        print(f"Rotating images {degrees} degrees")
+        if len(files) == 0:
+            return
+        while len(files) > 0:
+            file = files.pop(0)
+            conteiner_pb.visible = True
+            text_rotated_images_count.visible = True
+            text_select_images_count.visible = False
+            page.update()
+            try:
+                img = Image.open(file.path)
+                print(f"Image {file.name} opened")
+                width, height = img.size
+                if width < height:
+                    img.rotate(degrees, expand=True).save(file.path)
+                    print(f"Image {file.name} rotated {degrees} degrees")
+                    images_rotated_count += 1
+                text_rotated_images_count.value = f"{len(files)} imagens restantes - {images_rotated_count} imagens rotacionadas"
+                page.update()
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            time.sleep(0.1)
+
+        conteiner_pb.visible = False
+        text_rotated_images_count.visible = False
+
+        if images_rotated_count == 0:
+            return "Nenhuma imagem rotacionada!"
+        else:
+            return str(images_rotated_count) + " imagens rotacionadas com sucesso!"
+
+    def handle_click(e):
+        response = rotate_images(90)
+        open_dialog_modal(response)
+        files.clear()
+
+    def update_text_number_files():
+        text_select_images_count.visible = True
+        if len(files) == 0:
+            text_select_images_count.value = "Nenhuma imagem selecionada"
+            buttonRotate.disabled = True
+        elif len(files) >= 1:
+            text_select_images_count.value = f"{len(files)} imagens selecionadas"
+            buttonRotate.disabled = False
+        page.update()
+
+    def open_dialog_modal(content: str):
+        dlg_modal.content = ft.Text(content)
+        page.dialog = dlg_modal
+        dlg_modal.open = True
+        page.update()
 
     def pick_files_result(e: ft.FilePickerResultEvent):
         if e.files is None:
@@ -80,12 +124,7 @@ def main(page: ft.Page):
     pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
     page.overlay.append(pick_files_dialog)
 
-    textNumberFiles = ft.Text(
-        "Nenhuma imagem selecionada",
-        size=24,
-        color=ft.colors.GREY_500,
-        weight=ft.FontWeight.BOLD,
-    )
+
     buttonRotate = ft.OutlinedButton(
         "Rotacionar Imagens",
         width=500,
@@ -139,13 +178,25 @@ def main(page: ft.Page):
                 ),
                 ft.Row(
                     [
-                        textNumberFiles,
+                        text_select_images_count,
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
                 ),
                 ft.Row(
                     [
                         buttonRotate,
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                ),
+                ft.Row(
+                    [
+                        text_rotated_images_count,
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                ),
+                ft.Row(
+                    [
+                        conteiner_pb,
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
                 ),
